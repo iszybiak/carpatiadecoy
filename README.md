984591bb8be571be0a72ee9b48a3edc0f11e6101
Izabela Szybiak <is113782@stud.ur.edu.pl>
2022-08-01 16:06:50 +0200

    Base

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# mycase-app
+# Mycase-app
+The page with the attractions of Podkarpacie.
+
+## General info
+You can choose any photo of the attraction and see the description of the place and the location.
+
+## Technologies 
+Project is created with:
+* Vue/cli 5.0.4
+* Vuetify
 
 ## Project setup
 ```
---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container >
+    <v-toolbar flat >
+      <v-toolbar-title class="title" 
+      >Podkarpacie</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="btn-style" >
+        <v-btn text >
+          Strona główna
+        </v-btn>
+
+        <v-btn text>
+          Historia
+        </v-btn>
+
+        <v-btn text>
+          Zdjęcia
+        </v-btn>
+      </v-toolbar-items>
+
+      
+    </v-toolbar>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+
+    data: () => ({
+    }),
+  }
+</script>
+
+<style scoped>
+.title{
+  font-family: 'Garamond' !important;
+  font-size: 28px !important;
+}
+.btn-style{
+  font-family: 'Garamond' !important;
+}
+</style>
+
